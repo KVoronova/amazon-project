9a0906903b354f6d6f6080daa03c30c3480589dc
KVoronova <voronova.vs@gmail.com>
2024-08-19 20:26:54 +0300

    generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart = [];
+export const cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+}, {
+    productId: '8c9c52b5-5a19-4bcb-a5d1-158a74287c53',
+    quantity: 1,
+}];
+
 
 export function addToCatr (productId) {
     let matchingItem;
