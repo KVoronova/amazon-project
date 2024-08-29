4d33605075699e9f80f65490e791fba1d2afe99f
KVoronova <voronova.vs@gmail.com>
2024-08-29 22:09:34 +0300

    create tests with jasmine for cart

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,24 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
- cart = [{
-        productId: '8c9c52b5-5a19-4bcb-a5d1-158a74287c53',
-        quantity: 1,
-        deliveryOptionId: '1'
-    }];
+loadFromStorage();
+
+//START FOR TEST JASMINE
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+     cart = [{
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 1,
+            deliveryOptionId: '1'
+        }, {
+            productId: '77919bbe-0e56-475b-adde-4f24dfed3a04',
+            quantity: 1,
+            deliveryOptionId: '2'
+        }];
+    }
 }
-
-
+//END TEST JASMINE
 
 function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
