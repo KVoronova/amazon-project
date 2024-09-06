98507c9609f788464d93c525de8c6e7fe5188e89
KVoronova <voronova.vs@gmail.com>
2024-09-06 12:16:06 +0300

    practice promise and convert code to promise

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,9 +3,50 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
 //import "../data/backend-practice.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
 
-loadProducts(() => {
+]).then((values) => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value1');
+    });
+
+}).then((value) => {
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+  */
+
+/*
+loadProducts(() => {
+    loadCart(() => {
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+
+*/
